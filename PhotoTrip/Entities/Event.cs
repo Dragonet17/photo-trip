ccb2b98b33fc675ce6e9d9524a50f5fbf9942f53
Dragonet17 <karol.cichon@futurelife.com.pl>
2019-06-30 18:32:22 +0200

    Adding Event User Photo Entities and PhotoTripContex

---
+++ PhotoTrip/Entities/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PhotoTrip.Entities
+{
+    public class Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ PhotoTrip/Entities/Photo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PhotoTrip.Entities
+{
+    public class Photo
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
